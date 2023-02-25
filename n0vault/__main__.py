b52bede914c54766d75ee93cf399fb9200900d23
f552p69 <f552p69@gmail.com>
2023-02-25 15:24:34 +0400

    refactoring according to sonarcloud.io #3

--- n0vault/__main__.py
+++ n0vault/__main__.py
@@ -7,31 +7,25 @@ from n0vault import *
 if __name__ == "__main__":
     sys.tracebacklimit = 0
 
+    VAULT_FILE = (
+            VAULT_FILE_NAME
+            if (VAULT_FILE_NAME:=os.path.splitext(os.path.split(__file__)[1])[0]) not in ("__init__", "__main__")
+            else "default"
+    ) + ".n0vault"
     parser = argparse.ArgumentParser(prog = "n0vault")
-    parser.add_argument("-v", "--vault",    nargs=1,        
-        default=(
-            VAULT_FILE:=
-                (
-                    VAULT_FILE_NAME
-                    if (VAULT_FILE_NAME:=os.path.splitext(os.path.split(__file__)[1])[0]) not in ("__init__", "__main__")
-                    else "default"
-                )  + ".n0vault"
-        ),
-                                                          
-                                                            metavar="VAULT_FILE",    action='store',       help=f"use VAULT_FILE as storage. By default: '{VAULT_FILE}'")
-    parser.add_argument("-e", "--encrypt",                  default=None,            action='store_true',  help="save into ENCRYPTED vault file")
-    parser.add_argument("-p", "--password", nargs=1,                                                       help="use PASSWORD")
-    parser.add_argument("-d", "--decrypt",  dest="encrypt",                          action='store_false', help="save into DECRYPTED vault file")
-    parser.add_argument("-u", "--update",   nargs="*",      metavar=("XPATH","VALUE"), action='append',    help=
-        "add/update VALUE for the XPATH, if VALUE or both XPATH/VALUE are not defined, they should be entered manually without showing the VALUE")
-    parser.add_argument("-f", "--forbid",                   default=False,           action='store_true',  help="forbid to show/decrypt storage by standard functionality")
-    parser.add_argument("-r", "--remove",   nargs=1,        metavar="XPATH",         action='extend',      help="remove item with XPATH")
-    parser.add_argument("-s", "--show",     nargs=1,        metavar="XPATH",         action='extend',      help="show value for the XPATH")
-    
+    parser.add_argument("-v", "--vault",    nargs=1,        default=VAULT_FILE, metavar="VAULT_FILE",       action='store',         help="use VAULT_FILE as storage. By default: '" + VAULT_FILE + "'")
+    parser.add_argument("-e", "--encrypt",                  default=None,                                   action='store_true',    help="save into ENCRYPTED vault file")
+    parser.add_argument("-p", "--password", nargs=1,                                                                                help="use PASSWORD")
+    parser.add_argument("-d", "--decrypt",                                      dest="encrypt",             action='store_false',   help="save into DECRYPTED vault file")
+    parser.add_argument("-u", "--update",   nargs="*",                          metavar=("XPATH","VALUE"),  action='append',        help="add/update VALUE for the XPATH, if VALUE or both XPATH/VALUE are not defined, they should be entered manually without showing the VALUE")
+    parser.add_argument("-f", "--forbid",                   default=False,                                  action='store_true',    help="forbid to show/decrypt storage by standard functionality")
+    parser.add_argument("-r", "--remove",   nargs=1,                            metavar="XPATH",            action='extend',        help="remove item with XPATH")
+    parser.add_argument("-s", "--show",     nargs=1,                            metavar="XPATH",            action='extend',        help="show value for the XPATH")
+
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit()
-    
+
     args = parser.parse_args(sys.argv[1:])
 
     my_vault = n0Vault(vault_file_name = args.vault[0] if isinstance(args.vault, list) else args.vault if isinstance(args.vault, str) else None,
@@ -62,7 +56,7 @@ if __name__ == "__main__":
                 raise Exception(f"Expected 0..2 items list. Received {len_pair} items list {pair}")
         else:
             raise Exception(f"Expected list of lists. Received: {args.update}")
-            
+
         while True:
             value1 = getpass.getpass(f"Enter value assosiated with '{key}' (typed characters will not be shown):")
             if not value1:
@@ -74,7 +68,7 @@ if __name__ == "__main__":
                 else:
                     break
         my_vault.update({key:value1})
-        
+
     # ------------------------------------------------------
     # -r
     # ------------------------------------------------------
@@ -83,7 +77,7 @@ if __name__ == "__main__":
             my_vault.delete(key)
         else:
             print(f"Element with xpath '{key}' not exists, so it's not possible to remove.")
-            
+
         if args.encrypt is None:
             args.encrypt = True
             my_vault._encrypted = True
@@ -101,11 +95,11 @@ if __name__ == "__main__":
                         print(my_vault.show(start_xpath))
                 except:
                     print(f"Element with xpath '{start_xpath}' not exists, so it's not possible to show.")
-                
+
     if args.forbid:
         args.encrypt = True
         my_vault._encrypted = True
-        
-    if not args.encrypt is None:
+
+    if args.encrypt is not None:
         print(f"Saving '{my_vault.vault_file_name}' as %s%s..." % (["DECRYPTED", "ENCRYPTED"][int(my_vault._encrypted)], " (re-saving/updating/showing is forbidden)" if args.forbid else ""))
         my_vault.save(forbid_next_saving = args.forbid)
